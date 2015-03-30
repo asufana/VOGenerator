7c879943aa5a08d04810f4ee7985c62c1825b9ab
hana <asufanah@gmail.com>
2015-03-30 23:45:48 +0900

    Fixed project files.

---
+++ VOGenerator/src/test/resources/AbstractUserName.java
@@ -0,0 +1,17 @@
+package test;
+
+import javax.persistence.*;
+
+import lombok.*;
+import lombok.experimental.*;
+import play.modules.ddd.vo.*;
+
+import com.github.asufana.vogen.annotations.*;
+
+import domain.model.resource.apartment.*;
+import domain.model.resource.apartment.repositories.*;
+import domain.model.services.imports.model.csvrecord.vo.*;
+
+@VO
+public abstract class AbstractUserName {}
+
